339109a66e8a7d8cc743c4c9910014b7f93f7802
Rajneesh Kumar (Omnicom) <rajneesh.kumar1@omc.com>
2026-06-23 15:20:47 +0530

    Added Gemini client files

---
+++ app/gemini_client.py
@@ -0,0 +1,38 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+def ask_gemini(question):
+
+    dummy_data = """
+    Campaign A Spend = 12000
+    Campaign A Target = 10000
+
+    Campaign B Spend = 7000
+    Campaign B Target = 10000
+    """
+
+    prompt = f"""
+    You are a marketing analyst.
+
+    Dataset:
+    {dummy_data}
+
+    Question:
+    {question}
+
+    Provide a short business answer.
+    """
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return response.text
---
+++ app/test_gemini.py
@@ -0,0 +1,8 @@
+
+from gemini_client import ask_gemini
+
+response = ask_gemini(
+    "Which campaign is overpacing?"
+)
+
+print(response)
